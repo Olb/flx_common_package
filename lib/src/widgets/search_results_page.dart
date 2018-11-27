507c4da2bd991153ba691906b4c8c61c06b6cab6
billy <braybilly@gmail.com>
2018-11-27 17:06:42 +0200

    Updated search results page

--- lib/src/widgets/search_results_page.dart
+++ lib/src/widgets/search_results_page.dart
@@ -9,7 +9,6 @@ class SearchResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Results'),
@@ -17,28 +16,50 @@ class SearchResultsPage extends StatelessWidget {
       body: ListView.builder(
         itemCount: trainingList.length,
         itemBuilder: (itemBuilderCtx, index) {
-          return new ListTile(
-            leading: Image.network(trainingList[index].featuredImage),
-            title: new Text(trainingList[index].name),
-            subtitle:
-            new Text(trainingList[index].description),
-            onTap: () {
-              Training training = trainingList[index];
-              // Go straight to video if there is only one for the training
-              // either product or instruction
-              // TODO Figure out how to check for only one of everything
-              // TODO without using a massive if/else hell
-              // Use MVVM/MVP and generify the list of stuff going to
-              // product page, then use list.length to check for one
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (ctx) =>
-                          TrainingPage(training)));
-            },
-          );
+          return _listItem(trainingList[index]);
         },
       ),
+    );
+  }
+
+  Widget _listItem(Training training) {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Image.network(
+            training.featuredImage,
+            fit: BoxFit.contain,
+            height: 55.0,
+            width: 55.0,
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  training.name,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Text(
+                    training.description,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 12.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(height: 4,color: Colors.black,),
+      ],
     );
   }
 }
