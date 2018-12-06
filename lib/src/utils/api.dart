281aefc2ba91e15010afdfc9e2f114e8f12e7661
Lia <liavisatovici@gmail.com>
2018-12-06 16:02:04 +0200

    Added optional parameter byCategory for Api getProducts interface

--- lib/src/utils/api.dart
+++ lib/src/utils/api.dart
@@ -2,5 +2,5 @@ import 'package:flx_common_package/common.dart';
 
 abstract class Api {
   Future<dynamic> getTraining(User user);
-  Future<dynamic> getProducts(User user, String searchTerm);
+  Future<dynamic> getProducts(User user, String searchTerm, [bool byCategory=false]);
 }
