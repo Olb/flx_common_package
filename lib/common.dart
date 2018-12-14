9471abb6f9fb96abe908c3466a952775c1d21819
billy <braybilly@gmail.com>
2018-12-14 08:55:55 -0600

    commnetd out text icon button

--- lib/common.dart
+++ lib/common.dart
@@ -9,7 +9,7 @@ export 'package:flx_common_package/src/models/flx_generic_response.dart';
 export 'package:flx_common_package/src/models/user.dart';
 export 'package:flx_common_package/src/widgets/error_popup.dart';
 //export 'package:flx_common_package/src/widgets/search_popup.dart';
-export 'package:flx_common_package/src/widgets/text_icon_button.dart';
+//export 'package:flx_common_package/src/widgets/text_icon_button.dart';
 export 'package:flx_common_package/src/widgets/upload_icon.dart';
 export 'package:flx_common_package/src/widgets/loading_indicator.dart';
 export 'package:flx_common_package/src/widgets/loading_dialog.dart';
